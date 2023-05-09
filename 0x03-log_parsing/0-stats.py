d50490d2ac113b526254ee4a7888ddc920200f6d
David-Inkheart <okoliedave1@gmail.com>
2023-05-09 23:30:55 +0100

    reads stdin line by line and computes metrics

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,39 +3,33 @@
 import sys
 
 
-def print_stats(status, file_size):
-    """Print statistics for every 10 lines"""
+def print_stats(file_size, status_codes):
+    """Prints the statistics."""
     print("File size: {}".format(file_size))
-    for k in sorted(status.keys()):
-        if status[k]:
-            print("{}: {}".format(k, status[k]))
+    for key in sorted(status_codes):
+        if status_codes[key] != 0:
+            print("{}: {}".format(key, status_codes[key]))
 
 
-status = {'200': 0, '301': 0, '400': 0, '401': 0,
-          '403': 0, '404': 0, '405': 0, '500': 0}
-file_size = 0
-line_count = 0
+if __name__ == "__main__":
+    count = 0
+    file_size = 0
+    status_codes = {"200": 0, "301": 0, "400": 0,
+                    "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
 
-try:
-    for line in sys.stdin:
-        # Split the line into words based on space
-        words = line.split()
+    try:
+        for line in sys.stdin:
+            count += 1
+            split_line = line.split()
+            if len(split_line) == 9:
+                if split_line[8].isdigit():
+                    file_size += int(split_line[8])
+                if split_line[7] in status_codes:
+                    status_codes[split_line[7]] += 1
+            if count % 10 == 0:
+                print_stats(file_size, status_codes)
+        print_stats(file_size, status_codes)
 
-        # Get the file size and status code
-        file_size += int(words[-1])
-        status_code = words[-2]
-
-        # Check if the status code is valid
-        if status_code in status:
-            status[status_code] += 1
-
-        # Increment line count
-        line_count += 1
-
-        # Print stats for every 10 lines
-        if line_count % 10 == 0:
-            print_stats(status, file_size)
-
-except KeyboardInterrupt:
-    print_stats(status, file_size)
-    raise
+    except KeyboardInterrupt:
+        print_stats(file_size, status_codes)
+        raise
