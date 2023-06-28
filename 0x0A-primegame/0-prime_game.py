624bee45b4050d70a1e10e9c292c0472c6b6063b
David-Inkheart <okoliedave1@gmail.com>
2023-06-28 08:31:04 +0100

    refactored to capture edge cases

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -47,10 +47,10 @@ def isWinner(x, nums):
     Assuming Maria always goes first and both players play optimally,
     determine who the winner of each game is.
     """
-    # if not nums or x < 1:
-    #     return None
-    if any(num <= 0 for num in nums):
+    if not nums or x < 1:
         return None
+    # if any(num <= 0 for num in nums):
+    #     return None
     players = {'Maria': 0, 'Ben': 0}
     cluster = set()
     for elem in range(x):
